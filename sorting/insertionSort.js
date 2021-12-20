cbc4c613f9bf4aef9ba0a9b42a874e2f3d084900
funk93 <vojka1993@gmail.com>
2021-12-20 22:25:14 +0100

    insertion sort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,22 @@
+/**
+ * insertion sort
+ *
+ * builds up the sort by gradually creating a
+ * larger left half which is allways sorted
+ *
+ * Big O: O(n^2)
+ */
+
+function insertionSort(arr) {
+  var currentVal
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i]
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = currentVal
+  }
+  return arr
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4]))
